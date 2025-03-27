c9de34a70a6d5b992383d91e28121ab3194a5cf6
vingaming1113 <ilsoenielsen79@gmail.com>
2025-03-27 20:48:13 +0100

    fixed (I think)

--- commands/main/iptvprovider.js
+++ commands/main/iptvprovider.js
@@ -13,13 +13,11 @@ module.exports = {
             .setColor(0x00FF00)
             .setTitle('@iptvboy')
             .setDescription('send me and inbox');
-            .setFooter({ text: 'https://vxtwitter.com/iptvboy_' });
 
         const embed2 = new EmbedBuilder()
             .setColor(0x0000FF)
             .setTitle('@happyflower7458')
             .setDescription('[[FREE IPTV SERVICE, ONLY 4.99]]');
-            .setFooter({ text: 'https://vxtwitter.com/happyflower7458' });
 
         pagination.setEmbeds([embed1, embed2]);
         pagination.render();
