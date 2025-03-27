5a9feb1a5e2a81224e68010a6d177bdf3ba638b7
opuadm <opuadm@gmail.com>
2025-03-27 19:13:02 +0100

    Happy

--- commands/joke/happyphone.js
+++ commands/joke/happyphone.js
@@ -18,8 +18,14 @@ module.exports = {
             .setColor(0x0000FF)
             .setTitle('Happy Phone')
             .setDescription('Make it happy, just make it happy. Please make it happy and keep it happy. Bun + Metamask wallet has been drained because my trustworthy provider refused to recover my trusted shield wallet with a fox called metamask.');
+        
+        const embed3 = new EmbedBuilder()
+            .setColor(0x00FF00)
+            .setTitle('Happy')
+            .setDescription('If your device is happy, keep it happy. Do not make it sad or neutral or anything else than being Happy. It should ALWAYS be Happy.')
+            .setFooter({ text: 'Should always be happy' });
 
-        pagination.setEmbeds([embed1, embed2]);
+        pagination.setEmbeds([embed1, embed2, embed3]);
         pagination.render();
     },
 };
