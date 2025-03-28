430cf5760e52072fbafcd05ac2c328a7cf77e6cf
vingaming1113 <ilsoenielsen79@gmail.com>
2025-03-28 13:59:42 +0100

    added IPTV CHANNELS PROVIDER

--- commands/main/iptvprovider.js
+++ commands/main/iptvprovider.js
@@ -11,15 +11,20 @@ module.exports = {
 
         const embed1 = new EmbedBuilder()
             .setColor(0x00FF00)
-            .setTitle('@iptvboy')
+            .setTitle('cool boy(@iptvboy)')
             .setDescription('"send me and inbox"');
 
         const embed2 = new EmbedBuilder()
             .setColor(0x0000FF)
-            .setTitle('@happyflower7458')
+            .setTitle('happy flower(@happyflower7458)')
             .setDescription('"[[FREE IPTV SERVICE, ONLY 4.99]]"');
 
-        pagination.setEmbeds([embed1, embed2]);
+        const embed3 = new EmbedBuilder()
+            .setColor(0x0000FF)
+            .setTitle('IPTV CHANNELS PROVIDER(@Maxwell0845)')
+            .setDescription('"Hello, my tokens are still stuck on my old metamask account, i tried everything i could to transfer them to trust wallets coinbase but it just won\'t work, maybe it\'s because my iptv is too slow, can someone please help me"');
+
+        pagination.setEmbeds([embed1, embed2, embed3]);
         pagination.render();
     },
 };
